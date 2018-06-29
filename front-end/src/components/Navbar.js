38a7391bb764eb95e15020b578fa4ef27b47a8c8
Lcurvelo27 <lcurvelo27@gmail.com>
2018-06-29 16:16:55 -0600

    conditional navbar component

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,41 +1,54 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Navbar = (props) => {
+class Navbar extends Component {
+  render(){
 
-
-  return (
-    <nav className='nav-container'>
-      <div className='nav-content'>    
-      <Link to='/'>
-        <p>Logo/Home</p>
-      </Link>
-        { props.user &&
-          <div style={{display: 'flex', justifyContent: 'space-around', width: 200}}>
-            <Link to='/Search'>
-              <p>Search</p>
-            </Link> 
-            <Link to='/edit'>
-              <p>Account</p>
-            </Link>           
-            <Link to='/Logout'>
-              <p>Logout</p>
-            </Link>
-          </div>
-        }
-        { !props.user &&
-          <div style={{display: 'flex', justifyContent: 'space-around', width: 200}}>
-            <Link to='/search'>
-              <p>Search</p>
-            </Link>
-            <a href='http://localhost:3005/auth/login'>
-              <p>Login/Signup</p>
-            </a>
-          </div>
-        }
-      </div>
-    </nav>
-  )
+    let activeStyle = {
+      color: '#E3E38A'
+    }
+  
+    return (
+      <nav className='nav-container'>
+        <div className='nav-content'>    
+        <NavLink to='/'>
+          <p>Logo/Home</p>
+        </NavLink>
+          { this.props.user &&
+            <div style={{display: 'flex', justifyContent: 'space-around', width: 200}}>
+              <NavLink to='/Search' activeStyle={activeStyle}>
+                <p>Search</p>
+              </NavLink> 
+              <NavLink to='/edit' activeStyle={activeStyle}>
+                <p>Account</p>
+              </NavLink>           
+              <NavLink to='/Logout'>
+                <p>Logout</p>
+              </NavLink>
+            </div>
+          }
+          { !this.props.user &&
+            <div style={{display: 'flex', justifyContent: 'space-around', width: 200}}>
+              <NavLink to='/search' activeStyle={activeStyle}>
+                <p>Search</p>
+              </NavLink>
+              <a href='http://localhost:3005/auth/login'>
+                <p>Login/Signup</p>
+              </a>
+            </div>
+          }
+        </div>
+      </nav>
+    )
+  }
+}
+  
+const mapstateToProps = state => {
+  return {
+    user: state.user
+  }
 }
 
-export default Navbar
+export default connect(mapstateToProps)(Navbar)
+
