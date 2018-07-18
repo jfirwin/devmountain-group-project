e0f0082b89d11da713528212ca2ef7715af82d70
Lcurvelo27 <lcurvelo27@gmail.com>
2018-07-17 19:04:44 -0600

    added responsive style to search component

--- front-end/src/ducks/utils/animation.js
+++ front-end/src/ducks/utils/animation.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactTransition from 'react-addons-css-transition-group'
 
+const FirstChild = (props) => {
+    const childrenArray = React.Children.toArray(props.children)
+    return childrenArray[0] || null;
+}
+
 const ReactTransitionModule = (props) => {
     return (
         <ReactTransition
@@ -10,6 +15,7 @@ const ReactTransitionModule = (props) => {
         transitionAppear={true}
         transitionEnterTimeout={300}
         transitionLeaveTimeout={300}
+        component={FirstChild}
       >
       {props.children}
       </ReactTransition>
