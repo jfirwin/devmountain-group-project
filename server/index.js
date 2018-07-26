976a324d0b794d2e18767a07a1160d704b52e215
Foster Irwin <jfosterirwin@gmail.com>
2018-07-26 06:48:37 -0600

    added specificty to path

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: './env'})
+require('dotenv').config({path: path.join(__dirname,'./env')})
 const path = require('path')
 const express = require('express')
 const app = express()
