6f736b08fb74dd5bf3966bbf9a5e9bc951f94107
Foster Irwin <jfosterirwin@gmail.com>
2018-07-26 21:34:00 +0000

    server tweaks

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,10 @@
 const massive = require('massive')
+const path = require('path')
 
 module.exports = function(app) {
-  const connection = massive(process.env.CONNECTION_STRING)
+  const connection = massive(process.env.CONNECTION_STRING,{scripts: path.join(__dirname, '../db/')})
     .then(db => {
+console.log(db.search)
       console.log('db set up!')
       app.set('db', db)
     })
