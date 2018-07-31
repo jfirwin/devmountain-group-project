40e6f9f5fdde9911bf2e1277268fb46610efbdea
Lcurvelo27 <lcurvelo27@gmail.com>
2018-07-30 19:18:07 -0600

    fixed index.js

--- server/index.js
+++ server/index.js
@@ -3,12 +3,9 @@ const path = require('path')
 require('dotenv').config({path: path.join(__dirname,'.env')})
 const express = require('express')
 const app = express()
-const dbConnection = require('./config/db.js')(app)
-const middleware = require('./config/middleware.js')(app)
 const routes = require('./routes/router.js')(app)
 const authenthication = require('./routes/Authenthication')(app)
-const passport = require('passport')
-const Auth0Strategy = require('passport-auth0')
+
 
 
 app.use('/auth', authenthication)
