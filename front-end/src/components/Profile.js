5cc1c763cf3e0e7ae8de9e20417d91507376bbf5
Foster Irwin <jfosterirwin@gmail.com>
2018-08-09 22:43:19 -0600

    added logo to loading page

--- front-end/src/components/Profile.js
+++ front-end/src/components/Profile.js
@@ -5,6 +5,7 @@ import MissingPage from './MissingPage'
 import {getProfileDetails, setTheme} from '../ducks/action'
 import MediaQuery from 'react-responsive'
 import DefaultProfileMobile from './Themes/DefaultProfileMobile'
+import logo from '../images/cv-finder-black.svg'
 
 class FullScreenCv extends Component {
 
@@ -25,7 +26,9 @@ class FullScreenCv extends Component {
               {
           this.props.loading
           ?
-          <p>Loading...</p>
+          <div style={{height:'100vh',display:'flex',alignContent:'center',justifyContent:'center'}}>
+            <img src={logo} alt="CV Finder Logo" style={{width:"75vw"}}/>
+          </div>
           :
             this.props.user && this.props.user.skills
             ?
